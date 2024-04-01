ad8f0353fea975a0e4e34fcb277da6d98173a2ee
MetaException <67053613+MetaException@users.noreply.github.com>
2024-04-01 23:27:19 +0300

    rewrite configuration class

---
+++ api_client/Configuration/Configuration.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Serilog;
+
+namespace api_client.Configuration;
+
+public class Configuration
+{
+    public string FullPath { get; set; }
+    public RootSettings RootSettings { get; set; }
+
+    public Configuration(string FullPath, RootSettings RootSettings)
+    {
+        this.FullPath = FullPath;
+        this.RootSettings = RootSettings;
+    }
+
+    private void SaveJsonConfigChanges()
+    {
+        var serializedConfig = JsonConvert.SerializeObject(RootSettings, Formatting.Indented);
+        File.WriteAllText(FullPath, serializedConfig);
+    }
+
+    private void CreateConfigurationFile()
+    {
+        var directoryName = Path.GetDirectoryName(FullPath);
+
+        if (directoryName == null)
+        {
+            throw new ArgumentNullException(nameof(directoryName), $"{nameof(directoryName)} was null");
+        }
+
+        // Проверяем, существует ли папка
+        if (!Directory.Exists(directoryName))
+        {
+            Log.Information("Папка с конфигурацией не найдена.");
+            try
+            {
+                Log.Information("Создание папки с конфигурацией.");
+                Directory.CreateDirectory(directoryName);
+                Log.Information("Папка с конфигурацией создана.");
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Ошибка при создании папки с конфигурацией. {ex.ToString}");
+                Environment.Exit(1);
+            }
+        }
+
+        // Проверяем, существует ли файл
+        if (!File.Exists(FullPath))
+        {
+            Log.Information("Файл с конфигурацией не найден.");
+            try
+            {
+                Log.Information("Создание файла с конфигурацией.");
+                SaveJsonConfigChanges();
+                Log.Information("Файл с конфигурацией создан.");
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Ошибка при создании файла с конфигурацией. {ex.ToString}");
+                Environment.Exit(1);
+            }
+        }
+    }
+
+    public static Configuration SetupConfiguration(string fullPath = "Configuartion/appsettings.json")
+    {
+        Log.Information("Загрузка конфигурации.");
+
+        var configurationBuilder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(fullPath, optional: true, reloadOnChange: true);
+
+        var configuration = configurationBuilder.Build();
+
+        var rootSettings = configuration.Get<RootSettings>() ?? new RootSettings();
+
+        var newConfig = new Configuration(fullPath, rootSettings);
+        newConfig.CreateConfigurationFile();
+
+        return newConfig;
+    }
+}
--- api_client/Configuration/Settings.cs
+++ api_client/Configuration/Settings.cs
@@ -1,36 +1,35 @@
-﻿namespace api_client.Configuration
+﻿namespace api_client.Configuration;
+
+public class RootSettings
 {
-    public class RootSettings
-    {
-        public ConnectionServiceSettings ConnectionService { get; set; } = new ConnectionServiceSettings();
-        public LoggingSettings Logging { get; set; } = new LoggingSettings();
-        public ApiSettings API { get; set; } = new ApiSettings();
-    }
+    public ConnectionServiceSettings ConnectionService { get; set; } = new ConnectionServiceSettings();
+    public LoggingSettings Logging { get; set; } = new LoggingSettings();
+    public ApiSettings API { get; set; } = new ApiSettings();
+}
 
-    public class ConnectionServiceSettings
-    {
-        public ServerSettings Server { get; set; } = new ServerSettings();
+public class ConnectionServiceSettings
+{
+    public ServerSettings Server { get; set; } = new ServerSettings();
 
-        public int SendDelayIfNoConnection { get; set; } = 1000;
-        public int RecieveDelayIfNoConnection { get; set; } = 1000;
-        public int MonitoringDelay { get; set; } = 4000;
-        public int ResponseIterationDelay { get; set; } = 500;
+    public int SendDelayIfNoConnection { get; set; } = 1000;
+    public int RecieveDelayIfNoConnection { get; set; } = 1000;
+    public int MonitoringDelay { get; set; } = 4000;
+    public int ResponseIterationDelay { get; set; } = 500;
 
-        public class ServerSettings
-        {
-            public string Host { get; set; } = "127.0.0.1";
-
-            public int Port { get; set; } = 8888;
-        }
-    }
-
-    public class LoggingSettings
-    {
-        public int LogMaxSizeInBytes { get; set; } = 20000;
-    }
-    public class ApiSettings
+    public class ServerSettings
     {
         public string Host { get; set; } = "127.0.0.1";
-        public int Port { get; set; } = 8000;
+
+        public int Port { get; set; } = 8888;
     }
+}
+
+public class LoggingSettings
+{
+    public int LogMaxSizeInBytes { get; set; } = 20000;
+}
+public class ApiSettings
+{
+    public string Host { get; set; } = "127.0.0.1";
+    public int Port { get; set; } = 8000;
 }
